bdd213294ce2e6e3d601005fe27c49aaa33340ca
Shaykkoo <liyam78830@gmail.com>
2024-07-04 15:39:32 +0200

    user page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { UserPageComponent } from './user-page/user-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    /* ajouter les paths */
+    {path: 'myaccount', component: UserPageComponent},
+];
