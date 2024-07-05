b38fbb4e31ae9e97e91f259b576251d879161df8
rufus232 <mouakassarufus@gmail.com>
2024-07-05 18:28:37 +0200

    merge de la fonctionnalité register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,7 +7,7 @@ import { DetailsComponent } from './details/details.component';
 
 export const routes: Routes = [
 
-    { path: '', redirectTo: '/page-accueil', pathMatch: 'full' },
+    { path: '', redirectTo: '/register', pathMatch: 'full' },
     { path: 'page-accueil', component: PageAccueilComponent },
     { path: 'header', component: HeaderComponent},
     { path: 'product/:id', component: DetailsComponent},
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {NgIf} from "@angular/common";
-import {RouterLink} from "@angular/router";
+import { AuthentificationService } from '../authentification.service';
+import {Router, RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -21,8 +22,19 @@ export class RegisterComponent {
     email: new FormControl("", [Validators.required, Validators.email]),
     password: new FormControl("", [Validators.required, Validators.minLength(8)]),
   });
-
-  onSubmit(){
-    console.log(this.RegisterForm.value);
+  constructor(private authentificationService: AuthentificationService, private router: Router) {}
+  register() {
+    const postData = {
+      password: this.RegisterForm.value.password,
+      email: this.RegisterForm.value.email,
+      nom: this.RegisterForm.value.lastName,
+      prenom: this.RegisterForm.value.firstName
+    };
+    this.authentificationService.register(postData).subscribe((data: any) => {
+      console.log(data) ;
+      if(data.status == 'Inscription effectuée'){
+        this.router.navigate(['/login']);
+      }
+    });
   }
 }
