60871c2da670018b43c60b4acb4082a2a7973919
rufus232 <mouakassarufus@gmail.com>
2024-07-05 16:55:28 +0200

    Ajout des fonctionnalités login/register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { PageAccueilComponent } from './page-accueil/page-accueil.component';
 import { HeaderComponent } from './header/header.component';
 import { DetailsComponent } from './details/details.component';
+import { ContactComponent } from './contact/contact.component';
+import { PanierComponent } from './panier/panier.component';
 
 export const routes: Routes = [
 
@@ -9,8 +11,9 @@ export const routes: Routes = [
     { path: 'page-accueil', component: PageAccueilComponent },
     { path: 'header', component: HeaderComponent},
     { path: 'product/:id', component: DetailsComponent},
-    
-    // { path: 'pokemon-list', component: PokemonListComponent},
+    { path: 'contact', component: ContactComponent},
+    { path: 'panier', component: PanierComponent},
+
     // { path: 'favorites', component: FavoriteListComponent },
     // { path: 'login', component: LoginComponent },
     // { path: 'add-pokemon', component: AddPokemonComponent }
